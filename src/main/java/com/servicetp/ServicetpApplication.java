72534d3df80b4ae1ac805e6752ab3280775d30db
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2021-11-13 14:58:39 +0100

    Initial commit

---
+++ src/main/java/com/servicetp/ServicetpApplication.java
@@ -0,0 +1,13 @@
+package com.servicetp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicetpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicetpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/servicetp/ServicetpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicetp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicetpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
